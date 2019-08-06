7ff06364670b7246a6d9c13fb8f69bd39523bf44
Javier Collado <javier.collado@gmail.com>
2019-08-07 00:43:48 +0200

    Add validate_ip test cases

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,6 +1,9 @@
 import pytest
 
-from greynoise.util import validate_date
+from greynoise.util import (
+    validate_date,
+    validate_ip,
+)
 
 
 class TestValidateDate(object):
@@ -30,3 +33,31 @@ class TestValidateDate(object):
         """Invalid date values."""
         with pytest.raises(ValueError):
             validate_date(date)
+
+
+class TestValidateIP(object):
+
+    """IP validation test cases."""
+
+    @pytest.mark.parametrize(
+        'ip',
+        (
+            '0.0.0.0',
+            '255.255.255.255',
+            '192.168.1.0',
+        ),
+    )
+    def test_valid(self, ip):
+        """Valid ip address values."""
+        validate_ip(ip)
+
+    @pytest.mark.parametrize(
+        'ip',
+        (
+            '0.0.0.-1',
+            '255.255.255.256',
+            'not an ip address',
+        ),
+    )
+    def test_invalid(self, ip):
+        """Invalid ip address values."""
