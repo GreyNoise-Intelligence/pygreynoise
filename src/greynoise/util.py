ec11e393f1f8e52642eff513b13a5ad2290cd63a
Javier Collado <javier.collado@gmail.com>
2019-08-14 11:20:04 +0200

    Use empty string as api_key default value
    None is not allowed by the config parser object, so the empty string
    looks like the next sensible default.

--- src/greynoise/util.py
+++ src/greynoise/util.py
@@ -9,6 +9,7 @@ import appdirs
 from six.moves.configparser import ConfigParser
 
 
+CONFIG_FILE = os.path.join(appdirs.user_config_dir(), 'greynoise', 'config')
 LOGGER = logging.getLogger(__name__)
 
 
@@ -20,24 +21,20 @@ def load_config():
     :rtype: dict
 
     """
-    defaults = {'api_key': 'api key not provided'}
+    defaults = {'api_key': ''}
     if 'GREYNOISE_API_KEY' in os.environ:
         api_key = os.environ['GREYNOISE_API_KEY']
         LOGGER.debug('API key found in environment variable: %s', api_key)
         defaults['api_key'] = api_key
-    config_parser = ConfigParser(defaults)
 
-    config_file = os.path.join(appdirs.user_config_dir(), 'greynoise', 'config')
-    if os.path.isfile(config_file):
-        LOGGER.debug('Parsing configuration file: %s...', config_file)
-        config_parser.read(config_file)
-        if 'greynoise' not in config_parser:
-            raise ValueError(
-                'greynoise section not found in configuration file: {}'
-                .format(config_file)
-            )
+    config_parser = ConfigParser(defaults)
+    config_parser.add_section('greynoise')
+
+    if os.path.isfile(CONFIG_FILE):
+        LOGGER.debug('Parsing configuration file: %s...', CONFIG_FILE)
+        config_parser.read(CONFIG_FILE)
     else:
-        LOGGER.debug('Configuration file not found: %s', config_file)
+        LOGGER.debug('Configuration file not found: %s', CONFIG_FILE)
 
     return {
         'api_key': config_parser.get('greynoise', 'api_key')
