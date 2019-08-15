c5910c1e7c9b0de63ef36999861f7187fec5b4d4
Javier Collado <javier.collado@gmail.com>
2019-08-15 10:36:02 +0200

    Add main entry point test cases

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,14 +2,65 @@
 
 import argparse
 import datetime
+import textwrap
 
 import pytest
 
 from mock import Mock, patch
 
 from greynoise.cli import (
-    actors, context, multi_quick_check, noise, parse_arguments, quick_check, setup,
+    actors, context, multi_quick_check, main, noise, parse_arguments, quick_check,
+    setup,
 )
+
+
+class TestMain(object):
+    """Main entry point tests."""
+
+    def test_argv(self, capsys):
+        """sys.argv used if argv not explicitly passed."""
+        with patch('greynoise.cli.sys') as sys:
+            sys.argv = ['greynoise', '-h']
+
+            with pytest.raises(SystemExit):
+                main()
+
+            captured = capsys.readouterr()
+            assert captured.out.startswith('usage:')
+
+    @pytest.mark.parametrize(
+        'format_option, result, expected',
+        (
+            (
+                "json",
+                {"a": "result"},
+                '{"a": "result"}\n',
+            ),
+            (
+                "xml",
+                {"a": "result"},
+                textwrap.dedent(
+                    """\
+                    <?xml version="1.0" ?>
+                    <root>
+                    \t<a type="str">result</a>
+                    </root>
+
+                    """
+                )
+            ),
+        ),
+    )
+    def test_output_format(self, capsys, format_option, result, expected):
+        """Output is formatted."""
+        with patch('greynoise.cli.parse_arguments') as parse_arguments:
+            func = Mock(return_value=result)
+            args = argparse.Namespace(format=format_option, func=func)
+            parse_arguments.return_value = args
+
+            main()
+            captured = capsys.readouterr()
+            assert captured.out == expected
 
 
 class TestParseArguments(object):
