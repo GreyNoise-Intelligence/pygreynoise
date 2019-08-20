a40033c6277945cc14f93a1d9d66e0a3a3f4bb5d
Javier Collado <javier.collado@gmail.com>
2019-08-20 08:55:00 +0200

    Backport textwrap.indent to fix test cases in python2

--- tests/cli/test_subcommand.py
+++ tests/cli/test_subcommand.py
@@ -20,6 +20,27 @@ from greynoise.cli.subcommand import (
 from greynoise.util import CONFIG_FILE
 
 
+# Backported from python3 to make test cases work in python2
+def indent(text, prefix, predicate=None):
+    """Adds 'prefix' to the beginning of selected lines in 'text'.
+
+    If 'predicate' is provided, 'prefix' will only be added to the lines
+    where 'predicate(line)' is True. If 'predicate' is not provided,
+    it will default to adding 'prefix' to all non-empty lines that do not
+    consist solely of whitespace characters.
+    """
+    if predicate is None:
+
+        def predicate(line):
+            return line.strip()
+
+    def prefixed_lines():
+        for line in text.splitlines(True):
+            yield (prefix + line if predicate(line) else line)
+
+    return "".join(prefixed_lines())
+
+
 class TestSetup(object):
     """Setup subcommand test cases."""
 
@@ -140,7 +161,7 @@ class TestContext(object):
             ),
             (
                 "txt",
-                textwrap.indent(
+                indent(
                     textwrap.dedent(
                         """\
                                  OVERVIEW:
