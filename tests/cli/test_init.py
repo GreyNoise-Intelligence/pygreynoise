d8464d5bd113c5f908ef5c53a3c7c2ca882a5e48
Javier Collado <javier.collado@gmail.com>
2019-08-21 12:15:11 +0200

    Add test cases for the main command

---
+++ tests/cli/test_init.py
@@ -0,0 +1,28 @@
+"""CLI main command test cases."""
+from click.testing import CliRunner
+from mock import patch
+
+from greynoise.cli import main
+
+
+class TestMain(object):
+    """Main command tests."""
+
+    def test_no_arguments_passed(self):
+        """Main command fails if no arguments are passed."""
+        runner = CliRunner()
+        with patch("greynoise.cli.load_config") as load_config:
+            load_config.return_value = {"api_key": "<api_key>"}
+            result = runner.invoke(main, [])
+
+        assert result.exit_code == 2
+
+    def test_api_key_not_found(self):
+        """Main command fails if API key is not found."""
+        runner = CliRunner()
+        with patch("greynoise.cli.load_config") as load_config:
+            load_config.return_value = {"api_key": ""}
+            result = runner.invoke(main, [])
+
+        assert result.exit_code == -1
+        assert "Error: API key not found" in result.output
