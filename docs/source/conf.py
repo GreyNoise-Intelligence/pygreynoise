cd5b6c811e364a1e5b1ac9df8e4486e289724c7f
Javier Collado <javier.collado@gmail.com>
2019-08-28 19:52:34 +0200

    Bump version: 0.2.0 → 0.2.1

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2018-2019, GreyNoise Intelligence"
 author = "GreyNoise Intelligence"
 
 # The full version, including alpha/beta/rc tags
-release = "0.2.0"
+release = "0.2.1"
 
 
 # -- General configuration ---------------------------------------------------
--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="greynoise",
-    version="0.2.0",
+    version="0.2.1",
     description="Abstraction to interact with GreyNoise API.",
     url="https://greynoise.io/",
     author="GreyNoise Intelligence",
