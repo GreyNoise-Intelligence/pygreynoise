7f9076bb1af4e1d90673d5ca30523a51fd81ee77
Javier Collado <javier.collado@gmail.com>
2019-08-30 10:24:03 +0200

    Remove API get_actors method

--- src/greynoise/api.py
+++ src/greynoise/api.py
@@ -32,7 +32,6 @@ class GreyNoise(object):
     EP_NOISE_QUICK = "noise/quick/{ip_address}"
     EP_NOISE_MULTI = "noise/multi/quick"
     EP_NOISE_CONTEXT = "noise/context/{ip_address}"
-    EP_RESEARCH_ACTORS = "research/actors"
     UNKNOWN_CODE_MESSAGE = "Code message unknown: {}"
     CODE_MESSAGES = {
         "0x00": "IP has never been observed scanning the Internet",
@@ -205,17 +204,6 @@ class GreyNoise(object):
         if "ip" not in response:
             response["ip"] = ip_address
 
-        return response
-
-    def get_actors(self):
-        """Get the names and IP addresses of actors scanning the Internet.
-
-        :returns: Most labeled actors scanning the intenet.
-        :rtype: list
-
-        """
-        LOGGER.debug("Getting actors...")
-        response = self._request(self.EP_RESEARCH_ACTORS)
         return response
 
     def run_query(self, query):
