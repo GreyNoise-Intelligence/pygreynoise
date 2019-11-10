ee33c584976dc79805b8f9a890811428a4d9b01c
Javier Collado <javier.collado@tutanota.com>
2019-11-11 00:51:41 +0100

    Implement analyze API method

--- src/greynoise/api.py
+++ src/greynoise/api.py
@@ -1,5 +1,6 @@
 """GreyNoise API client."""
 
+import functools
 import re
 from collections import OrderedDict
 
@@ -74,7 +75,18 @@ class GreyNoise(object):
             octet=r"(?:(?:25[0-5])|(?:2[0-4]\d)|(?:1?\d?\d))"
         )
     )
+    ANALYZE_TEXT_CHUNK_SIZE = 10000
     FILTER_TEXT_CHUNK_SIZE = 10000
+
+    SECTION_KEY_TO_ELEMENT_KEY = {
+        "asns": "asn",
+        "categories": "category",
+        "classifications": "classification",
+        "countries": "country",
+        "operating_systems": "operating_system",
+        "organizations": "organization",
+        "tags": "tag",
+    }
 
     def __init__(self, api_key=None, timeout=None, use_cache=True):
         if api_key is None or timeout is None:
@@ -137,6 +149,81 @@ class GreyNoise(object):
             raise RequestFailure(response.status_code, body)
 
         return body
+
+    def analyze(self, text):
+        """Aggregate stats related to IP addresses from a given text.
+
+        :param text: Text input
+        :type text: str
+        :return: Aggregated stats for all the IP addresses found.
+        :rtype: dict
+
+        """
+        chunks = more_itertools.chunked(text, self.ANALYZE_TEXT_CHUNK_SIZE)
+        text_stats = {
+            "query": [],
+            "count": 0,
+            "stats": {},
+        }
+        chunks_stats = [self._analyze_chunk(chunk) for chunk in chunks]
+        functools.reduce(self._aggregate_stats, chunks_stats, text_stats)
+
+        # This maps section dictionaries to list of dictionaries
+        # (undoing mapping done previously to keep track of count values)
+        for section_key, section_value in text_stats["stats"].items():
+            section_element_key = self.SECTION_KEY_TO_ELEMENT_KEY[section_key]
+            text_stats["stats"][section_key] = [
+                {section_element_key: element_key, "count": element_count}
+                for element_key, element_count in section_value.items()
+            ]
+
+        return text_stats
+
+    def _analyze_chunk(self, text):
+        """Analyze chunk of lines that contain IP addresses from a given text.
+
+        :param text: Text input
+        :type text: str
+        :return: Iterator with stats for each one of the IP addresses found.
+        :rtype: dict
+
+        """
+        text_ip_addresses = set()
+        for input_line in text:
+            text_ip_addresses.update(self.IPV4_REGEX.findall(input_line))
+
+        chunk_stats = [self.stats(query=ip_address) for ip_address in text_ip_addresses]
+        return chunk_stats
+
+    def _aggregate_stats(self, accumulator, chunk_stats):
+        """Aggregate stats for different IP addresses.
+
+        :param accumulator: Aggregated stats for multiple IP addresses.
+        :type accumulator: dict
+        :param chunk_stats:
+            Stats for given chunk of text. These stats are not aggregated yet,
+            so they are a list of stats for each query made for that chunk.
+        :type chunk_stats: list(dict)
+
+        """
+        for query_stats in chunk_stats:
+            accumulator["query"].append(query_stats["query"])
+            accumulator["count"] += query_stats["count"]
+            for section_key, section_values in query_stats["stats"].items():
+                if section_values is None:
+                    continue
+                section_stats = accumulator["stats"].setdefault(section_key, {})
+
+                # This maps a list of dictionaries to a dictionary
+                # to easily keep track of counts.
+                section_element_key = self.SECTION_KEY_TO_ELEMENT_KEY[section_key]
+                for section_value in section_values:
+                    element_key = section_value[section_element_key]
+                    element_count = section_value["count"]
+                    section_stats.setdefault(element_key, 0)
+                    section_stats[element_key] += element_count
+
+        return accumulator
 
     def filter(self, text, noise_only=False):
         """Filter lines that contain IP addresses from a given text.
