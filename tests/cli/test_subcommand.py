7e1bf452c19c1b89250d4b0dbd8af69f7dd9d96c
Brad Chiappetta <brad@syncurity.net>
2020-12-15 14:41:50 -0500

    merge master, update tests

--- tests/cli/test_subcommand.py
+++ tests/cli/test_subcommand.py
@@ -424,7 +424,7 @@ class TestInteresting(object):
         """Interesting subcommand fails when ip_address is invalid."""
         runner = CliRunner()
 
-        expected = 'Error: Invalid value for "[IP_ADDRESS]...": not-an-ip\n'
+        expected = "Error: Invalid value for '[IP_ADDRESS]...': not-an-ip\n"
 
         result = runner.invoke(subcommand.interesting, ["not-an-ip"])
         assert result.exit_code == 2
@@ -553,7 +553,7 @@ class TestIP(object):
         """IP subcommand fails when ip_address is invalid."""
         runner = CliRunner()
 
-        expected = 'Error: Invalid value for "[IP_ADDRESS]...": not-an-ip\n'
+        expected = "Error: Invalid value for '[IP_ADDRESS]...': not-an-ip\n"
 
         result = runner.invoke(subcommand.ip, ["not-an-ip"])
         assert result.exit_code == 2
@@ -859,7 +859,7 @@ class TestQuick(object):
         """Quick subcommand fails when ip_address is invalid."""
         runner = CliRunner()
 
-        expected = 'Error: Invalid value for "[IP_ADDRESS]...": not-an-ip\n'
+        expected = "Error: Invalid value for '[IP_ADDRESS]...': not-an-ip\n"
 
         result = runner.invoke(subcommand.quick, ["not-an-ip"])
         assert result.exit_code == 2
@@ -974,7 +974,7 @@ class TestSetup(object):
     def test_missing_api_key(self):
         """Setup fails when api_key is not passed."""
         runner = CliRunner()
-        expected_error = 'Error: Missing option "-k" / "--api-key"'
+        expected_error = "Error: Missing option '-k' / '--api-key'"
 
         result = runner.invoke(subcommand.setup, [])
         assert result.exit_code == 2
