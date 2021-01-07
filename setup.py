f7ff61b60a7ff9afa2fef6275b874d13f2200078
Brad Chiappetta <38439955+bradchiappetta@users.noreply.github.com>
2021-01-07 12:40:44 -0500

    Bump version: 0.6.0 -> 0.7.0 (#402)

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ INSTALL_REQUIRES = [
 
 setup(
     name="greynoise",
-    version="0.6.0",
+    version="0.7.0",
     description="Abstraction to interact with GreyNoise API.",
     url="https://greynoise.io/",
     author="GreyNoise Intelligence",
