5352039b7364215f4684581c1c5bc2d354e6db32
Brad Chiappetta <38439955+bradchiappetta@users.noreply.github.com>
2025-06-18 16:37:38 -0400

    add metadata endpoint (#875)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ copyright = "2018-2025, GreyNoise Intelligence"
 author = "GreyNoise Intelligence"
 
 # The full version, including alpha/beta/rc tags
-release = "3.0.0"
+release = "3.0.1"
 
 
 # -- General configuration ---------------------------------------------------
